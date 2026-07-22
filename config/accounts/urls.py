cb924cdc5fc26b02ba6a2d60a27eb500cf278f58
adigun8 <adigun1988@gmail.com>
2026-07-22 19:18:37 +0100

    Updated congig/urls.py & accounts/urls.py files

--- config/accounts/urls.py
+++ config/accounts/urls.py
@@ -3,7 +3,7 @@ from .views import RegisterAPIView
 
 
 urlpatterns = [
-    path("api/v1/auth/register", RegisterAPIView.as_view(), name="register"),
+    path("register", RegisterAPIView.as_view(), name="register"),
 ]
 
 
